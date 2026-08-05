a9905b45c92d01f5123f90a0057fefb3246701a1
cocodeledev-boop <cocodeledev@gmail.com>
2026-08-05 21:52:28 +0200

    Add password mapping for classes
    Added password mapping for classes in the state.

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,10 @@ const ETABLISSEMENTS = {
   couleur: "#4F7FFF",
   motDePasse: "59500",
   classes: ["S2TMD" , "Seconde 4"],
+  motsDePasseClasses: {
+  "Seconde 4": "1234",
+  "S2TMD": "5678"
+},
   matieres: [
     { label: "Anglais",         color: "#5BB8F5", bg: "#EAF6FE" },
     { label: "Histoire-Géo",    color: "#27AE60", bg: "#E9F7EF" },
